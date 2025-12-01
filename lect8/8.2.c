6ce1c541a732fc59c5fec456d6b759e2a62784bc
nainadahiya <nainadahiya2006@gmail.com>
2025-12-01 16:54:12 +0530

    pattern

--- lect8/8.2.c
+++ lect8/8.2.c
@@ -49,16 +49,3 @@ int main(){
         printf("\n");
     }
 }
-//
-#include<stdio.h>
-int main(){
-    for(int i=0;i<3;i++){
-        for(int j=2;j>=0;j--){
-            printf(" ");
-        }
-        for(int k=2;k>=i;k--){
-            printf("*");
-        }
-        printf("\n");
-       }
-}
