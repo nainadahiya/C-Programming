8901a033c1bad737a087424dcd90c563bf913cca
nainadahiya <nainadahiya2006@gmail.com>
2026-04-14 20:21:31 +0530

    code

---
+++ lect26/26.1.c
@@ -0,0 +1,58 @@
+// topic: bits magic
+#include<stdio.h>
+int main(){
+    int n=4;
+    if (n&1)
+    {
+      printf("odd");
+    }
+    else{
+        printf("even");
+    }
+}
+
+//Bits Magic
+
+// Find even / odd through bitwise
+
+
+// It is more faster than usually
+
+
+#include<stdio.h>
+int main()
+{
+    int n;
+
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    if(n&1)
+    {
+        printf("\nOdd");
+
+    }
+    else
+    {
+        printf("\nEven");
+    }
+}
+
+#include<stdio.h>
+int main()
+{
+    int n;
+
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    if(n&1)
+    {
+        printf("\nOdd");
+
+    }
+    else
+    {
+        printf("\nEven");
+    }
+}
